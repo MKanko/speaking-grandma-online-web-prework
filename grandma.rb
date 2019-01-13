82a3ff984b4fc192a2a531fcea0537a537a754a8
Mark Kanko <mkanko66@gmail.com>
2019-01-13 05:28:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,15 +1,23 @@
 def speak_to_grandma(phrase)
-  response2 = "HUH?! SPEAK UP, SONNY"
-  response3 = "NO, NOT SINCE 1938!"
+  
   if phrase == "I LOVE YOU GRANDMA!"
+    
     return "I LOVE YOU TOO PUMPKIN!"
+    
     elsif phrase == phrase.downcase
+    
     return "HUH?! SPEAK UP, SONNY"
+    
     elsif phrase == phrase.downcase
+    
     return "HUH?! SPEAK UP, SONNY"
+    
     elsif phrase == phrase.upcase
+    
     return "NO, NOT SINCE 1938!"
-  else return 
+    
+  else return "NO, NOT SINCE 1938!"
+     
 end 
     
  
