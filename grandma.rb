812d01617028f068b6a7e6d0067f242d764f2ca1
Mark Kanko <mkanko66@gmail.com>
2019-01-13 06:45:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,9 +4,9 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     elsif phrase != phrase.upcase
-      return #{response1}
+      return "#{response1}"
     elsif phrase == phrase.upcase
-      return #{response2}
+      return "#{response2}"
   end
 end 
     
